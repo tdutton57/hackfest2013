6cd36b675c079538f97a3c714f6607caa01acabe
Matthew Quigley <matjosquig@gmail.com>
2013-07-14 20:17:34 -0500

    Added javascript and php support

--- app/assets/javascripts/cppSearch.js
+++ app/assets/javascripts/cppSearch.js
@@ -1,4 +1,13 @@
+$(document).ready(function(){
+	$("#searchForm").submit(function (e){
+		e.preventDefault();
+		$('#searchButton').click(); 
+		return false;
+	});
+});
+
 var htmlData;
+
 var urlSearch = function(){
 	var site = $(".btn-info.active").val();
 	var query = $("#search_query").val();
@@ -10,17 +19,20 @@ var urlSearch = function(){
 			var path ="getURL?search=http://api.jquery.com/?s="+query;
 			break;
 		case "JavaScript":
-			var path = "getURL?search=http://google.com/search?q=Javascript+"+query;
+			var path = "getURL?search=http://google.com/search?q="+query+"+domain:http://w3schools.com/js";
 			break;
-		default:
-			var path = "https://google.com";
+		case "PHP":
+			var path = "getURL?search=http://google.com/search?q="+query+"+domain:http://php.net/manual";
+			break;
 	}
 	$.ajax({
         type: 'GET',
         url: path, 
         complete: function (data) {
 			var text = data.responseText;
+			console.log(site);
 			if (site=="C++"){
+				$(".modal-body").html("<h2>No results found!</h2>");
 				var rx = new RegExp("//[ \\t\\r\\n]*API callback[ \\t\\r\\n]*google.search.Search.apiary[0-9]*\\(");
 				var rx2 = new RegExp("\\);[ \\t\\r\\n]*$");
 				var res = text.replace(rx, "");
@@ -44,19 +56,31 @@ var urlSearch = function(){
 				});
 			}
 			else if (site=="Jquery"){
+				$(".modal-body").html("<h2>No results found!</h2>");
+				$("#temp-content").remove();
 				$('body').append($("<div id='temp-content' style='display:none;'></div>"));
 				$("#temp-content").html($(data.responseText));
-				var dataUrl = $("#temp-content").find("article").first().find("h1.entry-title > a").attr("href");
+				var dataUrl = $("#temp-content").find("article").first().find("h3.r > a").attr("href");
 				$(".modal-body").load("getURL?search="+dataUrl+" .entry-content")
 			}
-			else if(site == "JavaScipt") {
+			else if(site=="JavaScript" || site=="PHP") {
+				$(".modal-body").html("<h2>No results found!</h2>");
+				$("#temp-content").remove();
+				console.log(text);
+				htmlData = text;
 				$('body').append($("<div id='temp-content' style='display:none;'></div>"));
-				$("#temp-content").html($(data.responseText));
-				console.log(data.responseText);
-				var dataUrl = $("#temp-content").find("article").first().find("h1.entry-title > a").attr("href");
-				$(".modal-body").load("getURL?search="+dataUrl+" .entry-content")
+				var rx = new RegExp(/<script[^>]*>/g);
+				$("#temp-content").html($(data.responseText.replace(rx, "")));
+				var dataUrl = $("#temp-content").find("#ires > ol > li.g > h3.r > a").first().attr("href");
+				dataUrl = dataUrl.replace("/url?q=", "");
+				console.log(dataUrl);
+				if (site=="JavaScript"){
+					$(".modal-body").load("getURL?search="+dataUrl+" #main");
+				} else if (site=="PHP"){
+					dataUrl = dataUrl.replace(/\&sa\=.*/, "")
+					$(".modal-body").load("getURL?search="+dataUrl+" .refentry");
+				}
 			}
-			//$(".modal-body").load("getURL?search="+encodeURIComponent(dataUrl));
 		}
     });
 }
